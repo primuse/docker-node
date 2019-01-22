34a4d08561f35481c367e3a2679a2578ee15f400
Tikuochukwu Okoye <okoyetiku@gmail.com>
2019-01-22 16:38:57 +0100

    [Feature #163389856] Create Signup page

--- src/components/auth/login.jsx
+++ src/components/auth/login.jsx
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
-import { LoginForm } from "../forms/loginForm.jsx";
-import { Link } from 'react-router-dom';
-import '../../css/modules.css';
+import React, { Component } from "react";
+import { LoginForm, SignupForm } from "../forms/loginForm.jsx";
+import { Link } from "react-router-dom";
+import "../../css/modules.css";
 import "../../css/style.css";
 import "../../css/login.css";
 
@@ -18,14 +18,15 @@ export const Login = () => {
         </ul>
         <ul className="ml-auto mt-25">
           <li>
-            <Link to ="/signup">Signup</Link>
+            <Link to="/signup">Signup</Link>
           </li>
         </ul>
       </header>
       <div className="banner">
         <div className="sendit-text mb-34">
           <h5>
-            The <span className="head-span">Courier</span> service app you've been waiting for
+            The <span className="head-span">Courier</span> service app you've
+            been waiting for
           </h5>
           <p>SendIT makes reaching the world easier!</p>
         </div>
@@ -33,4 +34,41 @@ export const Login = () => {
       </div>
     </section>
   );
-}
+};
+
+export const Signup = () => {
+  return (
+    <section id="login">
+      <header>
+        <ul>
+          <li id="head-brand">
+            <Link to="/">
+              Send<span class="head-span">IT</span>
+            </Link>
+          </li>
+        </ul>
+        <ul class="ml-auto mt-25">
+          <li>
+            <Link to="/">Login</Link>
+          </li>
+        </ul>
+      </header>
+      <div class="banner">
+        <div class="sendit-text">
+          <h5>
+            The <span class="head-span">Courier</span> service app you've been
+            waiting for
+          </h5>
+          <p class="mb-34">
+            Register to experience our world class courier services
+          </p>
+        </div>
+        <div id="form-box">
+          <div class="text-center">
+            <SignupForm />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
--- src/components/forms/loginForm.jsx
+++ src/components/forms/loginForm.jsx
@@ -1,32 +1,113 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
 export class LoginForm extends Component {
-  state = { 
-    email: '',
-    password: ''
-  }
+  state = {
+    email: "",
+    password: ""
+  };
 
-  onChange = (event) =>{
-    this.setState({[event.target.name]: event.target.value});
-  }
+  onChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
 
-
-  render() { 
+  render() {
     const { email, password } = this.state;
-    return ( 
+    return (
       <form id="login-form" className="text-center">
         <div className="form-row">
-          <input type="email" id="email" name="email" value= {email} placeholder="Email" required onChange={this.onChange} />
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={email}
+            placeholder="Email"
+            required
+            onChange={this.onChange}
+          />
         </div>
         <div className="form-row">
-          <input type="password" id="pwd" name="password" value={password} placeholder="Password" required onChange={this.onChange} />
+          <input
+            type="password"
+            id="pwd"
+            name="password"
+            value={password}
+            placeholder="Password"
+            required
+            onChange={this.onChange}
+          />
         </div>
-        <button type="submit" className="btn lg is-outlined mb-14">Login</button>
+        <button type="submit" className="btn lg is-outlined mb-14">
+          Login
+        </button>
         <div>
-          <Link to='/reset'>Forgot Password?</Link>
+          <Link to="/reset">Forgot Password?</Link>
         </div>
       </form>
-     );
+    );
+  }
+}
+
+export class SignupForm extends Component {
+  state = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: ""
+  };
+
+  onChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  render() {
+    const { firstName, lastName, email, password } = this.state;
+    return (
+      <form id="signupForm">
+        <div class="column is-two form-row">
+          <input
+            type="text"
+            name="firstName"
+            value={firstName}
+            id="firstName"
+            placeholder="First name"
+            required
+            onChange={this.onChange}
+          />
+          <input
+            type="text"
+            name="lastName"
+            value={lastName}
+            id="lastName"
+            placeholder="Last name"
+            required
+            onChange={this.onChange}
+          />
+        </div>
+        <div class="column is-two form-row">
+          <input
+            type="email"
+            name="email"
+            value={email}
+            id="email"
+            placeholder="Email"
+            required
+            onChange={this.onChange}
+          />
+          <input
+            type="password"
+            name="password"
+            value={password}
+            id="password"
+            placeholder="Password"
+            required
+            onChange={this.onChange}
+          />
+        </div>
+        <button id="signup" class="btn lg is-outlined">
+          Signup
+        </button>
+      </form>
+    );
   }
 }
