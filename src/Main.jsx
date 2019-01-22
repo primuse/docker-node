21dc58eb9e1cd3f25162003b624b0a3d9c951d12
Tikuochukwu Okoye <okoyetiku@gmail.com>
2019-01-22 16:24:03 +0100

    [Feature #163389843] Create Login page

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,2 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Login } from "../src/components/auth/login.jsx";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+const MainApp = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Login} />
+      </Switch>
+    </Router>
+  );
+};
+
+ReactDOM.render(<MainApp />, document.getElementById("app"));
+
+export default MainApp;
