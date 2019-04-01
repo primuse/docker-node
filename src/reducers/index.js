91132503b9033876ead1b5a1691219f8756d5af8
Tikuochukwu Okoye <okoyetiku@gmail.com>
2019-04-01 15:52:11 +0100

    feat: logout user

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,10 +2,21 @@ import { combineReducers } from 'redux';
 import authReducer from './authReducer';
 import parcelReducer from './parcelReducer';
 import userReducer from './userReducer';
+import { LOG_OUT } from '../actions/actionTypes';
 
 
-export default combineReducers({
+const appReducer = combineReducers({
   auth: authReducer,
   parcel: parcelReducer,
   users: userReducer,
 });
+
+const rootReducer = (state, action) => {
+  if (action.type === LOG_OUT) {
+    localStorage.clear();
+    state = undefined;
+  }
+  return appReducer(state, action);
+};
+
+export default rootReducer;
